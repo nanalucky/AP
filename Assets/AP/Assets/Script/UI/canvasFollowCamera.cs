c826305ad7a13003f21b1d2548dac6fd45a4a40e
nanalucky <nana_lucky@msn.com>
2019-04-06 11:00:25 +0800

    canvas follow camera

---
+++ Assets/AP/Assets/Script/UI/canvasFollowCamera.cs
@@ -0,0 +1,22 @@
+﻿// Description : infoUI : This script is used to display a UI box that contains infos on screen 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvasFollowCamera: MonoBehaviour {
+
+    private Camera cameraFollow;
+    private Matrix4x4 canvasCamMatrix;
+
+	void Awake(){
+        cameraFollow = gameObject.GetComponent<Canvas>().worldCamera;
+        canvasCamMatrix = cameraFollow.worldToCameraMatrix * gameObject.transform.localToWorldMatrix;
+	}
+
+    void Update()
+    {
+        gameObject.transform.FromMatrix(cameraFollow.cameraToWorldMatrix * canvasCamMatrix);
+    }
+
+}
