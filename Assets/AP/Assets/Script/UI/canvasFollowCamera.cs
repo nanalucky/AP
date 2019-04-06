15aa4a39d2903e6af343294de097198c6d661bd2
nanalucky <nana_lucky@msn.com>
2019-04-07 01:26:21 +0800

    canvas: world to camera space

--- Assets/AP/Assets/Script/UI/canvasFollowCamera.cs
+++ Assets/AP/Assets/Script/UI/canvasFollowCamera.cs
@@ -6,13 +6,17 @@ using UnityEngine.UI;
 
 public class canvasFollowCamera: MonoBehaviour {
 
+    public float distance = 20.0f;
     private Camera cameraFollow;
-    private Matrix4x4 canvasCamMatrix;
+    private Matrix4x4 canvasCamMatrix = new Matrix4x4();
 
 	void Awake(){
         cameraFollow = gameObject.GetComponent<Canvas>().worldCamera;
-        canvasCamMatrix = cameraFollow.worldToCameraMatrix * gameObject.transform.localToWorldMatrix;
-	}
+        Matrix4x4 matCanvas = new Matrix4x4();
+        Vector3 posCanvas = cameraFollow.transform.position + cameraFollow.transform.forward.normalized * distance;
+        matCanvas.SetTRS(posCanvas, gameObject.transform.rotation, gameObject.transform.localScale);
+        canvasCamMatrix = cameraFollow.worldToCameraMatrix * matCanvas;
+    }
 
     void Update()
     {
