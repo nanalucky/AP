cfaba653460c05534cc8bc15d30f42134304dca7
nanalucky <nana_lucky@msn.com>
2019-04-07 20:35:02 +0800

    delete player when load new scene

---
+++ Assets/AP/Assets/Script/UI/VRDestroy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class VRDestroy : MonoBehaviour
+{
+    void OnDestroy()
+    {
+        Destroy(GameObject.Find("Player"));
+    }
+}
